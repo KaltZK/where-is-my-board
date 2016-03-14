4b222dbd3c9dba5b236a968863cdd94f4ca38262
kzk-board <715480954@qq.com>
2016-03-14 17:19:43 +0800

    2016年 03月 14日 星期一 17:19:43 CST

--- update-ip.sh
+++ update-ip.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 addr=$(LANG=en_US.ASCII ifconfig wlan1 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 echo "<h1>Board's IP: $addr</h1>" > index.html
+git add .
+git commit -m "$(date)"
+git push origin gh-pages 
